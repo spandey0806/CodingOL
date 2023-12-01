275f90ae71811e8ce5e173ced1f29dbf75301a65
Shubham Pandey <shubhampandey0806@gmail.com>
2023-12-01 13:14:08 +0530

    Sort user according to thier age descending

--- UsersName.js
+++ UsersName.js
@@ -1,8 +1,8 @@
 // 1: Write code to get an array of names from given array of users
 const users = [
-{id: 1, name: 'Jack', isActive: true},
-{id: 2, name: 'John', isActive: true},
-{id: 3, name: 'Mike', isActive: false},
+{id: 1, name: 'Jack', isActive: true , age :20},
+{id: 2, name: 'John', isActive: true ,age :18},
+{id: 3, name: 'Mike', isActive: false , age :30},
 ]
 
 // Using For Loop
@@ -33,3 +33,10 @@ const users = [
 
 const activeUsers =  users.filter((user)=> user.isActive).map((user)=> user.name);
 console.log(activeUsers);
+
+//3. Sort users by age descending
+
+const namesUser = users.sort((a,b) => b.age  - a.age)
+                        .map((user)=> user.name);
+console.log(namesUser);
+
